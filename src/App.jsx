4baf85f5dea07d470d88589f853547c862433092
kyssiei <kyssiei.kay@gmail.com>
2025-03-11 22:56:42 -0400

    made updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import { Route, Routes } from "react-router-dom";
 import Currencies from "./pages/currencies";
 import Main from "./pages/main";
 import Price from "./pages/price";
+import Nav from "./components/nav";
 
 function App() {
 
 
   return (
     <div className="App">
+      <Nav />
       <Routes>
         <Route path="/" element={<Main/>}/>
         <Route path="/currencies" element={<Currencies/>}/>
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import ReactDOM from "react-dom";
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter as Router} from "react-router-dom";
 
-// Import BrowserRouter and rename it to Router
-import { BrowserRouter as Router } from "react-router-dom";
-
-// Wrap the App component with the Router component to enable the router features.
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
+   <Router>
       <App />
     </Router>
-  </StrictMode>,
-  document.getElementById("root")
-);
-
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  </StrictMode>
 );
